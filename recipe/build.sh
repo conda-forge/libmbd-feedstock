d5ee1985b00e9d5e74f77951ad0f34b2500c398f
Sebastian Ehlert <28669218+awvwgk@users.noreply.github.com>
2021-12-02 16:57:01 +0100

    Add support for ELSI
    MNT: Re-rendered with conda-build 3.21.6, conda-smithy 3.15.0, and conda-forge-pinning 2021.12.02.13.07.22

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,8 +3,10 @@ set -ex
 
 if [ "${mpi}" != "nompi" ]; then
   MPI=ON
+  ELSI_LIBRARIES="$(pkg-config elsi --libs)"
 else
   MPI=OFF
+  ELSI_LIBRARIES=""
 fi
 
 if [ "${mpi}" == "openmpi" ]; then
@@ -17,6 +19,8 @@ cmake_options=(
    "-DCMAKE_INSTALL_PREFIX=${PREFIX}"
    "-DCMAKE_INSTALL_LIBDIR=lib"
    "-DENABLE_SCALAPACK_MPI=${MPI}"
+   "-DENABLE_ELSI=${MPI}"
+   "-DELSI_LIBRARIES=${ELSI_LIBRARIES}"
    "-DLAPACK_LIBRARIES=lapack;blas"
    "-DSCALAPACK_LIBRARIES=scalapack"
    "-GNinja"
